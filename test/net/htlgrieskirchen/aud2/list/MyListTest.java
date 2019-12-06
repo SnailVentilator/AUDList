142f8c321651a25c85862bea4087c50d8a64db7f
Andreas <aigner.andi05@gmail.com>
2019-12-06 10:20:50 +0000

    Test syntax

--- test/net/htlgrieskirchen/aud2/list/MyListTest.java
+++ test/net/htlgrieskirchen/aud2/list/MyListTest.java
@@ -13,5 +13,45 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author fabian
  */
 public class MyListTest {
+    @Test 
+    public boolean add(String s) {
+        
+    }
+    @Test
+    public void add(int index, String s) {
+
+    }
+    @Test
+    public String get(int index) {
+
+    }
+    @Test
+    public boolean remove(String s) {
+
+    }
+    @Test
+    public String remove(int index) {
+
+    }
+    @Test
+    public String set(int index, String s) {
+
+    }
+    @Test
+    public boolean contains(String s) {
+
+    }
+    @Test
+    public int indexOf(String s) {
+
+    }
+    @Test
+    public int size() {
+
+    }
+    @Test
+    public boolean isEmpty() {
+        
+    }
     
 }
