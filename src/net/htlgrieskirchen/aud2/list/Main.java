83ea6325e4f708f273d5109f62855256d0e64fcc
Fabian <fabian.paul.gurtner@gmail.com>
2020-01-10 11:18:04 +0100

    Add first benchmark

--- src/net/htlgrieskirchen/aud2/list/Main.java
+++ src/net/htlgrieskirchen/aud2/list/Main.java
@@ -5,6 +5,7 @@
  */
 package net.htlgrieskirchen.aud2.list;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -14,11 +15,33 @@ import java.util.Random;
 public class Main {
 
     private static Random random = new Random();
-    private static final char[] CHARACTERS = {};
+    private static final char[] CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
 
     private static String generateRandomString(int length) {
         return random.ints(length, 0, CHARACTERS.length).map(i -> CHARACTERS[i])
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
     }
 
+    public static void main(String[] args) {
+        Timer timer;
+        MyList<String> myList = new MyList<>();
+        ArrayList<String> arrayList = new ArrayList<>();
+        
+        String[] testStrings = new String[100000];
+        for (int i = 0; i < testStrings.length; i++) {
+            testStrings[i] = generateRandomString(128);
+        }
+        
+        timer = new Timer("[MyList] Insert 100.000 random strings");
+        for (String testString : testStrings) {
+            myList.add(testString);
+        }
+        timer.end();
+        
+        timer = new Timer("[ArrayList] Insert 100.000 random strings");
+        for (String testString : testStrings) {
+            arrayList.add(testString);
+        }
+        timer.end();
+    }
 }
---
+++ src/net/htlgrieskirchen/aud2/list/Timer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor. Brigitte.
+ */
+package net.htlgrieskirchen.aud2.list;
+
+/**
+ *
+ * @author fabian
+ */
+public class Timer {
+    private final long startTime;
+    private final String title;
+    public Timer(String title) {
+        this.title = title;
+        this.startTime = System.currentTimeMillis();
+    }
+    
+    public void end() {
+        System.out.format("%s: %dms\n", title, System.currentTimeMillis() - startTime);
+    }
+}
