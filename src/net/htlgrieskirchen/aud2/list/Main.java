3a575ba58ab2aa8e14e64bfc01c8aaa9ccd1fdf1
Fabian <fabian.paul.gurtner@gmail.com>
2020-01-29 07:25:28 +0100

    Add LinkedList benchmarks to Main method

--- src/net/htlgrieskirchen/aud2/list/Main.java
+++ src/net/htlgrieskirchen/aud2/list/Main.java
@@ -6,6 +6,7 @@
 package net.htlgrieskirchen.aud2.list;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Random;
 
 /**
@@ -23,23 +24,19 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        BenchmarkFlexer.flex();
-        System.exit(0);
+        //BenchmarkFlexer.flex();
+        //System.exit(0);
 
         Timer timer;
-        MyList<String> myList = new MyList<>();
         ArrayList<String> arrayList = new ArrayList<>();
+        MyList<String> myList = new MyList<>();
+        LinkedList<String> linkedList = new LinkedList<>();
+        MyLinkedList<String> myLinkedList = new MyLinkedList<>();
 
         String[] testStrings = new String[100000];
         for (int i = 0; i < testStrings.length; i++) {
             testStrings[i] = generateRandomString(128);
         }
-
-        timer = new Timer("[MyList] Insert 100.000 random strings");
-        for (String testString : testStrings) {
-            myList.add(testString);
-        }
-        timer.end();
 
         timer = new Timer("[ArrayList] Insert 100.000 random strings");
         for (String testString : testStrings) {
@@ -47,10 +44,21 @@ public class Main {
         }
         timer.end();
 
-        //TODO: choose random strings
-        timer = new Timer("[MyList] Search 10.000 random strings");
-        for (int i = 0; i < 10000; i++) {
-            myList.contains(testStrings[i]);
+        timer = new Timer("[MyList] Insert 100.000 random strings");
+        for (String testString : testStrings) {
+            myList.add(testString);
+        }
+        timer.end();
+
+        timer = new Timer("[LinkedList] Insert 100.000 random strings");
+        for (String testString : testStrings) {
+            linkedList.add(testString);
+        }
+        timer.end();
+
+        timer = new Timer("[MyLinkedList] Insert 100.000 random strings");
+        for (String testString : testStrings) {
+            myLinkedList.add(testString);
         }
         timer.end();
 
@@ -60,15 +68,45 @@ public class Main {
         }
         timer.end();
 
-        timer = new Timer("[MyList] Remove 10.000 random strings");
+        timer = new Timer("[MyList] Search 10.000 random strings");
         for (int i = 0; i < 10000; i++) {
-            myList.remove(testStrings[i]);
+            myList.contains(testStrings[i]);
+        }
+        timer.end();
+
+        timer = new Timer("[LinkedList] Search 10.000 random strings");
+        for (int i = 0; i < 10000; i++) {
+            linkedList.contains(testStrings[i]);
+        }
+        timer.end();
+
+        timer = new Timer("[MyLinkedList] Search 10.000 random strings");
+        for (int i = 0; i < 10000; i++) {
+            myLinkedList.contains(testStrings[i]);
         }
         timer.end();
 
         timer = new Timer("[ArrayList] Remove 10.000 random strings");
         for (int i = 0; i < 10000; i++) {
             arrayList.remove(testStrings[i]);
+        }
+        timer.end();
+
+        timer = new Timer("[MyList] Remove 10.000 random strings");
+        for (int i = 0; i < 10000; i++) {
+            myList.remove(testStrings[i]);
+        }
+        timer.end();
+
+        timer = new Timer("[LinkedList] Remove 10.000 random strings");
+        for (int i = 0; i < 10000; i++) {
+            linkedList.remove(testStrings[i]);
+        }
+        timer.end();
+
+        timer = new Timer("[MyLinkedList] Remove 10.000 random strings");
+        for (int i = 0; i < 10000; i++) {
+            myLinkedList.remove(testStrings[i]);
         }
         timer.end();
     }
